5601b2a51db91f324a2fe542740349c0e19986c2
Gover-W <alemanjuan741@gmail.com>
2024-05-24 15:32:39 -0500

    se agreo un boton para guardar en una api de prueba

--- app/src/main/java/com/example/api/MainActivity.java
+++ app/src/main/java/com/example/api/MainActivity.java
@@ -82,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
         rq.add(myRquest);
     }
-
     public void respuestaGuardar(JSONObject respuesta){
 
         try {
